662a868b92f9dc679258b73d2acde6f3a12d937f
Samiur R Khan <samkhan@zhangweb.med.umich.edu>
2018-02-02 19:33:20 -0500

    Move code to a src/ directory

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// Protein Distance Profile Main
+// Program to read proteins and generate the histogram version of the
+// protein distance profile
+// Will use gnuplotiostream here to plot histograms
+// Will use protein.h and pdb_io.h to read in protein data and place into
+// histogram
+// Will use cartesian.h to calculate the distances
---
+++ src/protein_test.cpp
@@ -0,0 +1,16 @@
+// Unit tests for the Protein library
+// Test data and PDB files are located in the test-data directory
+
+#include <cassert>
+
+void test_filename_constructor();
+
+int main() {
+    test_filename_constructor();
+    return 0;
+}
+
+// Method to test that a protein file was properly read.
+void test_filename_constructor() {
+
+}
